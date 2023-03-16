b30ce0a1e5f850a636ce9b9b0e444fb514a8516a
edgargaticaCU <88678635+edgargaticaCU@users.noreply.github.com>
2023-03-16 15:36:46 -0500

    Correction to several attribute type ids.

--- parser.py
+++ parser.py
@@ -35,7 +35,7 @@ def get_attribute_list(blob, attribute_type_id) -> list:
 def get_evidence_list(supporting_studies) -> list:
     evidence_list = []
     for study in supporting_studies:
-        publication = get_attribute_object(study["attributes"], "biolink:supporting_document")["value"]
+        publication = get_attribute_object(study["attributes"], "biolink:publications")["value"]
         score = get_attribute_object(study["attributes"], "biolink:extraction_confidence_score")["value"]
         sentence = get_attribute_object(study["attributes"], "biolink:supporting_text")["value"]
         subject_span = get_attribute_object(study["attributes"], "biolink:subject_location_in_text")["value"]
@@ -78,7 +78,7 @@ def load_data(data_folder):
             if line[0] not in entity_dict.keys() or line[2] not in entity_dict.keys():
                 continue
             attributes_blob = json.loads(line[-1])
-            supporting_studies = get_attribute_list(attributes_blob, 'biolink:supporting_study_result')
+            supporting_studies = get_attribute_list(attributes_blob, 'biolink:has_supporting_study_result')
             evidences = get_evidence_list(supporting_studies)
             subject_parts = line[0].split(':')
             object_parts = line[2].split(':')
@@ -112,7 +112,7 @@ def load_data(data_folder):
                 },
                 "association": {
                     "edge_label": edge_label,
-                    "evidence_count": get_attribute_object(attributes_blob, "biolink:has_evidence_count")["value"],
+                    "evidence_count": get_attribute_object(attributes_blob, "biolink:evidence_count")["value"],
                     "evidence": evidences,
                     "edge_attributes": json.loads(line[-1])
                 },
